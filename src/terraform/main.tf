db13140ac0b78fb1d87f6df872dd50df081ff601
Matheus <contato.matheuslimams@gmail.com>
2026-04-18 03:49:25 -0300

    feat:pipeline test

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -27,28 +27,28 @@ module "vpc" {
 }
 
 # EKS Kubernetes Cluster (Elastic Kubernetes Service)
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "21.18.0"
+# module "eks" {
+#   source  = "terraform-aws-modules/eks/aws"
+#   version = "21.18.0"
 
-  name               = var.aws_eks_cluster_name
-  kubernetes_version = var.aws_eks_version
+#   name               = var.aws_eks_cluster_name
+#   kubernetes_version = var.aws_eks_version
 
-  enable_cluster_creator_admin_permissions = true
+#   enable_cluster_creator_admin_permissions = true
 
-  subnet_ids = module.vpc.private_subnets
-  vpc_id     = module.vpc.vpc_id
+#   subnet_ids = module.vpc.private_subnets
+#   vpc_id     = module.vpc.vpc_id
 
-  endpoint_public_access = true
+#   endpoint_public_access = true
 
-  eks_managed_node_groups = {
-    default = {
-      min_size       = 2
-      max_size       = 2
-      desired_size   = 2
-      instance_types = var.aws_eks_managed_node_groups_instance_types
-      tags           = var.aws_project_tags
-    }
-  }
-  tags = var.aws_project_tags
-}
+#   eks_managed_node_groups = {
+#     default = {
+#       min_size       = 2
+#       max_size       = 2
+#       desired_size   = 2
+#       instance_types = var.aws_eks_managed_node_groups_instance_types
+#       tags           = var.aws_project_tags
+#     }
+#   }
+#   tags = var.aws_project_tags
+# }
